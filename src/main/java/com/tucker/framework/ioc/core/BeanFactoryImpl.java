dd7f0d74668df8fd31c9649cf643a189a1ac9d38
tonytan233 <tonytancanbebetter@gmail.com>
2022-08-08 23:36:34 +0800

    update BeanFactoryImpl

--- src/main/java/com/tucker/framework/ioc/core/BeanFactoryImpl.java
+++ src/main/java/com/tucker/framework/ioc/core/BeanFactoryImpl.java
@@ -1,7 +1,6 @@
 package com.tucker.framework.ioc.core;
 
 import com.tucker.framework.ioc.bean.BeanDefinition;
-import com.tucker.framework.ioc.utils.ClassUtils;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -18,22 +17,9 @@ public class BeanFactoryImpl implements BeanFactory{
     private static final ConcurrentHashMap<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
 
     private static final Set<String> beanNameSet = Collections.synchronizedSet(new HashSet<>());
-    /**
-     * @param name String
-     * @return Object
-     * @throws Exception
-     */
+
     @Override
     public Object getBean(String name) throws Exception {
         return null;
-    }
-
-    private Object createBean(BeanDefinition beanDefinition) throws Exception{
-        String beanName = beanDefinition.getClassName();
-        Class bean = ClassUtils.loadClass(beanName);
-        if(bean == null) {
-            throw new Exception("System cannot find bean by class name.");
-        }
-
     }
 }
