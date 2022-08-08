de73bee445024d67135164e3aa5c6f593d772ed2
tonytan233 <tonytancanbebetter@gmail.com>
2022-08-09 00:01:20 +0800

    update BeanFactoryImpl

--- src/main/java/com/tucker/framework/ioc/core/BeanFactoryImpl.java
+++ src/main/java/com/tucker/framework/ioc/core/BeanFactoryImpl.java
@@ -1,11 +1,14 @@
 package com.tucker.framework.ioc.core;
 
 import com.tucker.framework.ioc.bean.BeanDefinition;
+import com.tucker.framework.ioc.bean.ConstructArg;
+import com.tucker.framework.ioc.utils.BeanUtils;
+import com.tucker.framework.ioc.utils.ClassUtils;
+import java.lang.reflect.Constructor;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author tucker
@@ -21,5 +24,31 @@ public class BeanFactoryImpl implements BeanFactory{
     @Override
     public Object getBean(String name) throws Exception {
         return null;
+    }
+
+    private Object createBean(BeanDefinition beanDefinition) throws Exception {
+        String beanName = beanDefinition.getClassName();
+        Class bean = ClassUtils.loadClass(beanName);
+        if(bean == null){
+            throw new Exception("we cannot find bean by bean's class name");
+        }
+        List<ConstructArg> constructArgList = beanDefinition.getConstructArgList();
+        if(constructArgList != null && !constructArgList.isEmpty()){
+            List<Object> objectList = new ArrayList<>();
+            constructArgList.stream().map( o -> {
+                if(o.getValue() != null){
+                    objectList.add(o.getValue());
+                }else {
+                    objectList.add(getBean(o.getName()));
+                }
+                return ;
+            });
+            Class[] constructOrArgTypes = objectList.stream().map(o -> o.getClass())
+                    .collect(Collectors.toList()).toArray(new Class[]{});
+            Constructor constructor = bean.getConstructor(constructOrArgTypes);
+            return BeanUtils.instanceByCglib(bean, constructor, objectList.toArray());
+        } else {
+            return BeanUtils.instanceByCglib(bean, null, null);
+        }
     }
 }
